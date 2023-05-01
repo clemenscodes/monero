cc20e4487bad58e80905373b101945b1438e2907
Clemens Horn <clemens.horn@mni.thm.de>
2023-05-01 14:34:21 +0200

    chore: add xmrig config

---
+++ scripts/xmrig.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+script_path="$(realpath "$0")"
+script_dir="$(dirname "$script_path")"
+root_dir="$(dirname "$script_dir")"
+
+xmrig \
+    -o 127.0.0.1:3333 \
+    -c "$root_dir/config/xmrig.json" \
+    -l "$root_dir/logs/xmrig.log" \
+    -api-worker-id miner \
+    --http-host 127.0.0.1 \
+    --http-port 3334
