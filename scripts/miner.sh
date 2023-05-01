c28203f3309f0207303a0fdf0982b619d35fd691
Clemens Horn <clemens.horn@mni.thm.de>
2023-05-01 18:36:46 +0200

    feat: improve hashrate by 25%

--- scripts/miner.sh
+++ scripts/miner.sh
@@ -4,10 +4,11 @@ script_path="$(realpath "$0")"
 script_dir="$(dirname "$script_path")"
 root_dir="$(dirname "$script_dir")"
 
-xmrig \
+sudo xmrig \
     -o 127.0.0.1:3333 \
     -c "$root_dir/config/xmrig.json" \
     -l "$root_dir/logs/xmrig.log" \
+    --randomx-1gb-pages \
     --http-host 127.0.0.1 \
     --http-port 3334 \
     -B
