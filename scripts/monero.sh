3e9ed4e7bad410c4d9374f090174c5dc5218706e
Clemens Horn <clemens.horn@mni.thm.de>
2023-05-01 12:26:47 +0200

    fix: commands work with fixed hosts file

--- scripts/monero.sh
+++ scripts/monero.sh
@@ -7,9 +7,4 @@ root_dir="$(dirname "$script_dir")"
 monerod \
     --data-dir="$root_dir/data" \
     --log-file="$root_dir/logs/monerod.log" \
-    --confirm-external-bind \
-    --p2p-bind-ip=127.0.0.1 \
-    --p2p-bind-port=18080 \
-    --rpc-bind-ip=127.0.0.1 \
-    --rpc-bind-port=18081 \
     --detach
--- scripts/wallet.sh
+++ scripts/wallet.sh
@@ -5,5 +5,4 @@ script_dir="$(dirname "$script_path")"
 root_dir="$(dirname "$script_dir")"
 
 monero-wallet-cli \
-    --daemon-host 127.0.0.1 \
     --log-file="$root_dir/logs/wallet.log"
